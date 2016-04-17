4310e91bdcf337121493f865ae4bf16bd57180a6
cjhoward92 <CHoward@aspen-systems.com>
2016-04-17 14:39:05 -0700

    Initial Commit

---
+++ SimpleCache.Tests/CacheFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace SimpleCache.Tests
+{
+    public class CacheFactoryTests
+    {
+        [Fact]
+        public void Create_Success()
+        {
+            CacheFactory facotry = new CacheFactory();
+            var cache = facotry.Create(new CacheConfigurationOptions());
+            Assert.NotNull(cache);
+        }
+
+        [Fact]
+        public void Create_NullOptions()
+        {
+            CacheFactory facotry = new CacheFactory();
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                facotry.Create(null);
+            });
+            Assert.IsType<ArgumentNullException>(ex);
+        }
+    }
+}
---
+++ SimpleCache.Tests/CacheTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace SimpleCache.Tests
+{
+    public class CacheTests
+    {
+        [Fact]
+        public void Cache_Success()
+        {
+            Cache cache = new Cache();
+            int key = 1;
+            string value = "derp";
+            CacheEntry entry = new CacheEntry(key, value);
+            cache.Cache(entry);
+            Assert.True(cache.NumberOfElements == 1);
+        }
+    }
+}
